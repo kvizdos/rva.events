1db54c0b8a4a53a3facdd9eaadcdf4be5e67ea87
Kenton Vizdos <kvizdos@protonmail.com>
2025-06-17 17:40:28 -0400

    Add dynamic OG images

--- go.mod
+++ go.mod
@@ -3,26 +3,36 @@ module github.com/kvizdos/rva.events
 go 1.24.3
 
 require (
-	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/chromedp/chromedp v0.13.6
+	github.com/fogleman/gg v1.3.0
+	github.com/kvizdos/easyblog v1.7.1
+)
+
+require (
+	github.com/BurntSushi/toml v1.5.0 // indirect
 	github.com/alecthomas/chroma/v2 v2.2.0 // indirect
+	github.com/chromedp/cdproto v0.0.0-20250403032234-65de8f5d025b // indirect
+	github.com/chromedp/sysutil v1.1.0 // indirect
 	github.com/dlclark/regexp2 v1.7.0 // indirect
-	github.com/fogleman/gg v1.3.0 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
+	github.com/go-json-experiment/json v0.0.0-20250211171154-1ae217ad3535 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.4.0 // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/golobby/cast v1.3.3 // indirect
 	github.com/golobby/config/v3 v3.4.2 // indirect
 	github.com/golobby/dotenv v1.3.2 // indirect
 	github.com/golobby/env/v2 v2.2.4 // indirect
-	github.com/kvizdos/easyblog v1.7.0 // indirect
 	github.com/mangoumbrella/goldmark-figure v1.2.0 // indirect
 	github.com/stefanfritsch/goldmark-fences v1.0.0 // indirect
-	github.com/yuin/goldmark v1.7.8 // indirect
+	github.com/yuin/goldmark v1.7.11 // indirect
 	github.com/yuin/goldmark-highlighting/v2 v2.0.0-20230729083705-37449abec8cc // indirect
 	github.com/yuin/goldmark-meta v1.1.0 // indirect
 	go.abhg.dev/goldmark/anchor v0.2.0 // indirect
 	go.abhg.dev/goldmark/toc v0.11.0 // indirect
-	golang.org/x/image v0.25.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/image v0.27.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
 	gopkg.in/yaml.v2 v2.3.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"math"
 	"net/url"
 	"slices"
 	"time"
 
+	"github.com/fogleman/gg"
 	"github.com/kvizdos/easyblog/builder"
 	"github.com/kvizdos/easyblog/entrypoint"
 )
@@ -56,8 +58,91 @@ func getSoonEvents(posts builder.PostList) builder.PostList {
 	return out
 }
 
+func findMaxFontSize(dc *gg.Context, fontPath, text string, maxWidth, maxHeight float64) float64 {
+	minSize := 10.0
+	maxSize := 300.0
+	var bestSize float64
+
+	for i := 0; i < 10; i++ { // 10 iterations = good enough precision
+		mid := (minSize + maxSize) / 2
+		if err := dc.LoadFontFace(fontPath, mid); err != nil {
+			break
+		}
+		lines := dc.WordWrap(text, maxWidth)
+		height := mid * 1.15 * float64(len(lines))
+
+		if height > maxHeight {
+			maxSize = mid
+		} else {
+			bestSize = mid
+			minSize = mid
+		}
+	}
+
+	return bestSize
+}
+
+func GenerateOG(postTitle string, outPath string, config builder.OGImageConfig) {
+	const width = 1200
+	const height = 630 // Common OG image size
+
+	dc := gg.NewContext(width, height)
+	// Set background color
+	dc.SetRGB(244.0/255.0, 239.0/255.0, 229.0/255.0)
+	dc.Clear()
+
+	padding := 40.0
+
+	boxWidth := float64(width) - 2*padding
+	boxHeight := float64(height) - 200 // leave space for image/footer etc
+
+	// Load a custom font
+	fontSize := findMaxFontSize(dc, config.FontPath, postTitle, boxWidth, boxHeight)
+	if err := dc.LoadFontFace(config.FontPath, fontSize); err != nil {
+		panic(err)
+	}
+
+	// The text to be drawn (top of the image)
+	text := postTitle
+	dc.SetRGB(46/255.0, 74/255.0, 98/255.0)
+	wrapped := dc.WordWrap(text, float64(width)-2*padding)
+	lineHeight := fontSize * 1.15
+	totalTextHeight := lineHeight * float64(len(wrapped))
+	footerImageHeight := 80.0
+	startY := ((float64(height)-footerImageHeight)-totalTextHeight)/2 + lineHeight/2
+	centerX := float64(width) / 2
+	for i, line := range wrapped {
+		y := startY + float64(i)*lineHeight
+		dc.DrawStringAnchored(line, centerX, y, 0.5, 0.5) // center align horizontally
+	}
+
+	// Draw the circular headshot image
+	img, err := gg.LoadImage(config.IconPath)
+	if err != nil {
+		panic(err)
+	}
+
+	targetX := float64(width) / 2
+
+	imgW := float64(img.Bounds().Dx())
+	imgH := float64(img.Bounds().Dy())
+	scale := 100.0 / math.Max(imgW, imgH) // fit into 100px size
+
+	targetY := float64(height) - (imgH * scale) - padding
+
+	dc.Push()
+	dc.Translate(targetX, targetY)
+	dc.Scale(scale, scale)
+	dc.Translate(-imgW/2, 0) // center it horizontally
+	dc.DrawImage(img, 0, 0)
+	dc.Pop()
+	// Save the result
+	dc.SavePNG(outPath)
+}
+
 func main() {
 	entrypoint.Start(entrypoint.EasyblogOpts{
+		CustomOGGenerator: GenerateOG,
 		CustomFuncs: template.FuncMap{
 			"RangeEvents":   rangeEvents,
 			"GetSoonEvents": getSoonEvents,
